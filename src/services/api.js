4881d59ae96730125b11fc995b52685fa3b3860e
lucero <luc4r0.ch4ngr4.99@gmail.com>
2025-10-28 10:54:45 -0500

    se elimino imagenes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = 'http://localhost:3001/backend_api';
-// export const API_BASE_URL = 'https://www.crecemos.com.pe/backend_api';
+// export const API_BASE_URL = 'http://localhost:3001/backend_api';
+export const API_BASE_URL = 'https://www.crecemos.com.pe/backend_api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
